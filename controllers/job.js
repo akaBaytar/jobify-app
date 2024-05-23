a94d1c3cec94b8dbb5cb8555f90f19b10f0648df
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-23 16:24:11 +0300

    job controller

---
+++ controllers/job.js
@@ -0,0 +1,64 @@
+import { StatusCodes } from 'http-status-codes';
+
+import Job from '../models/job.js';
+
+// POST
+// api/v1/jobs
+const createJob = async (req, res) => {
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+// GET
+// api/v1/jobs
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
+  res.status(StatusCodes.OK).json({ jobs });
+};
+
+// GET
+// api/v1/jobs/:id
+const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = await Job.findById(id);
+
+  if (!job) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `There is no job with id ${id}.` });
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
+
+// PATCH
+// api/v1/jobs/:id
+const updateJob = async (req, res) => {
+  const { id } = req.params;
+  const job = await Job.findByIdAndUpdate(id, req.body, { new: true });
+
+  if (!job) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `There is no job with id ${id}.` });
+  }
+
+  res.status(StatusCodes.OK).json({ msg: 'Job updated succesfully.' });
+};
+
+// DELETE
+// api/v1/jobs/:id
+const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const job = await Job.findByIdAndDelete(id);
+
+  if (!job) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `There is no job with id ${id}.` });
+  }
+
+  res.status(StatusCodes.OK).json({ msg: 'Job deleted succesfully.' });
+};
+
+export { createJob, getAllJobs, getJob, updateJob, deleteJob };
