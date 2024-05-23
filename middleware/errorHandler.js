1f963953369faa5b96f1398383aa58b0f6a17081
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-23 18:09:50 +0300

    handling cast error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,5 +4,11 @@ export const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const message = err.message || 'An error occurred.';
 
+  if (err.name === 'CastError') {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ name: err.name, msg: 'An error occured.' });
+  }
+
   res.status(statusCode).json({ name: err.name, msg: message });
 };
