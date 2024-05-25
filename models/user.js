dda7a722605bdc1f35b6e5f32d3eb96b01b6fa43
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-25 13:29:55 +0300

    user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+
+  lastname: {
+    type: String,
+  },
+
+  email: {
+    type: String,
+  },
+
+  password: {
+    type: String,
+  },
+
+  location: {
+    type: String,
+  },
+
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export default mongoose.model('User', User);
