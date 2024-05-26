88b7e207d8f1e12fc9c0ca6ef3cfbd35c9d95a0b
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-26 12:52:44 +0300

    logout controller and router

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,8 @@ import { hashPassword, checkPassword, createJWT } from '../utilities/index.js';
 
 import { Unauthorized } from '../error/index.js';
 
+// POST
+// /api/v1/auth/register
 const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
@@ -17,6 +19,8 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: 'Registered successfully.' });
 };
 
+// POST
+// /api/v1/auth/login
 const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
 
@@ -36,4 +40,12 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Logged in successfully.' });
 };
 
-export { register, login };
+// GET
+// /api/v1/auth/logout
+const logout = (req, res) => {
+  res.cookie('token', '', { httpOnly: true, expires: new Date(Date.now()) });
+
+  res.status(StatusCodes.OK).json({ msg: 'Logged out successfully.' });
+};
+
+export { register, login, logout };
--- router/auth.js
+++ router/auth.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { login, register } from '../controllers/auth.js';
+import { login, register, logout } from '../controllers/auth.js';
 import { validateUserInput, validateUser } from '../middleware/validation.js';
 
 const router = Router();
 
+router.get('/logout', logout);
 router.post('/login', validateUser, login);
 router.post('/register', validateUserInput, register);
 
