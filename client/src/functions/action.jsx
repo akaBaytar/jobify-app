f7e8109fd323b3506d3c31b3425f18a2f22e67ea
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-28 14:53:12 +0300

    edit job action

--- client/src/functions/action.jsx
+++ client/src/functions/action.jsx
@@ -24,6 +24,17 @@ export const registerAction = action('/auth/register', 'Register', '/login');
 export const loginAction = action('/auth/login', 'Login', '/dashboard');
 export const addJobAction = action('/jobs', 'Job added', 'all-jobs');
 
-export const editJobAction = async () => {
-  return null;
+export const editJobAction = async ({ request, params }) => {
+  const formData = await request.formData();
+
+  const data = Object.fromEntries(formData);
+
+  try {
+    await fetch.patch(`/jobs/${params.id}`, data);
+    toast.success(`Job edited successfully.`);
+    return redirect(`/dashboard/all-jobs`);
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
