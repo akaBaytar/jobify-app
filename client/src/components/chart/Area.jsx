0d75dbb41e05fbbc692936e560c52451f3d0fcaf
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-29 22:44:40 +0300

    charts components

--- client/src/components/chart/Area.jsx
+++ client/src/components/chart/Area.jsx
@@ -1,7 +1,32 @@
 import PropTypes from 'prop-types';
 
-const Area = () => {
-  return <div>Area</div>;
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area as Chart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+
+const Area = ({ data }) => {
+  return (
+    <ResponsiveContainer width={'100%'} height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray={'3 3'} />
+        <XAxis dataKey={'date'} />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Chart
+          type={'monotone'}
+          dataKey={'count'}
+          stroke='#8b5cf6'
+          fill='#c4b5fd'
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
 Area.propTypes = {
--- client/src/components/chart/Bar.jsx
+++ client/src/components/chart/Bar.jsx
@@ -1,13 +1,31 @@
 import PropTypes from 'prop-types';
 
-const Bar = () => {
-  return <div>Bar</div>;
-};
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar as Chart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
+const Bar = ({ data }) => {
+  return (
+    <ResponsiveContainer width={'100%'} height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray={'3 3'} />
+        <XAxis dataKey={'date'} />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Chart type={'monotone'} dataKey={'count'} fill='#c4b5fd' />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 Bar.propTypes = {
   data: PropTypes.array,
 };
-
 
 export default Bar;
