e8a1b904acfa796eea825923970954b1acc06487
Burak Bilgili <burak.bilgili@icloud.com>
2024-05-31 14:06:51 +0300

    initialize react query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import router from './router';
-
 import { checkDefaultTheme } from './utilities/darkTheme';
+import {queryClient} from './helpers/query'
 
 checkDefaultTheme();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
